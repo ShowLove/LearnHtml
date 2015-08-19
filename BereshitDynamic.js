f3914dac50ebef7224bc4cf200766c2f6141f308
Carlos Garzon <showlove@knights.ucf.edu>
2015-08-19 19:38:54 -0400

    changed id to a string instead of a number

--- BereshitDynamic.js
+++ BereshitDynamic.js
@@ -1,10 +1,12 @@
+ 
+ //gets idElement from html 
  function changeText(idElement) {
-    if( document.getElementById('bereshit').innerHTML == "In the begining"){
+    if( document.getElementById('idElement').innerHTML == "In the begining"){
       document.getElementById('bereshit').innerHTML ='בְּרֵאשִׁית';
       return false;
     }
 
-    if(idElement==1){
+    if('idElement'=="bereshit"){
         document.getElementById('bereshit').innerHTML ='In the begining';
         return false;
     }
