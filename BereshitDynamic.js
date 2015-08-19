03741abad2e3b4f2f6ca34c2edccd6e1b0131b7d
Carlos Garzon <showlove@knights.ucf.edu>
2015-08-19 19:03:13 -0400

    able to change words onclick

---
+++ BereshitDynamic.js
@@ -0,0 +1,13 @@
+ function changeText(idElement) {
+    if(idElement==1){
+        document.getElementById('bereshit').innerHTML ='In the begining';
+        return false;
+    }
+ }
+
+$(document).ready(main);
+
+//function showPanel(fieldName) {
+// var fieldNameElement = document.getElementById('field_name');
+
+//    $('.bershit').update('In ("the") beginning');  didn't work
