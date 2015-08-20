ba7c1071b2b9b768e0313092a203d72b204dab21
Carlos Garzon <showlove@knights.ucf.edu>
2015-08-20 01:03:30 -0400

    got two words to switch back and forth

--- Bereshit/BereshitDynamic.js
+++ Bereshit/BereshitDynamic.js
@@ -1,11 +1,19 @@
  function changeText(idElement) {
-    if( document.getElementById('bereshit').innerHTML == "In the begining"){
+    if( idElement == 1   &&  document.getElementById('bereshit').innerHTML == "In the begining"){
       document.getElementById('bereshit').innerHTML ='בְּרֵאשִׁית';
+      return false;
+    }
+    if( idElement == 2  &&  document.getElementById('bara').innerHTML == "created"){
+      document.getElementById('bara').innerHTML ='בָּרָא';
       return false;
     }
 
     if( idElement == 1 ){
         document.getElementById('bereshit').innerHTML ='In the begining';
+        return false;
+    }
+    if( idElement == 2 ){
+        document.getElementById('bara').innerHTML ='created';
         return false;
     }
  }
