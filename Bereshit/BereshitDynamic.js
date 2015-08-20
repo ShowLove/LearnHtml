5992b3dbf6cf6ec47c4b8470aea76e96fa1aa35f
Carlos Garzon <showlove@knights.ucf.edu>
2015-08-20 01:22:07 -0400

    all words now switch between hebrew and english

--- Bereshit/BereshitDynamic.js
+++ Bereshit/BereshitDynamic.js
@@ -7,6 +7,26 @@
       document.getElementById('bara').innerHTML ='בָּרָא';
       return false;
     }
+    if( idElement == 3  &&  document.getElementById('Elohim').innerHTML == "Elohim"){
+      document.getElementById('Elohim').innerHTML ='אֱלֹהִים';
+      return false;
+    }
+    if( idElement == 4  &&  document.getElementById('et').innerHTML == "אֵת"){
+      document.getElementById('et').innerHTML ='אֵת';
+      return false;
+    }
+    if( idElement == 5  &&  document.getElementById('hashamaim').innerHTML == "the heavens"){
+      document.getElementById('hashamaim').innerHTML ='הַשָּׁמַיִם';
+      return false;
+    }
+    if( idElement == 6  &&  document.getElementById('veet').innerHTML == "and אֵת"){
+      document.getElementById('veet').innerHTML ='וְאֵת';
+      return false;
+    }
+    if( idElement == 7  &&  document.getElementById('haaretz').innerHTML == "the earth"){
+      document.getElementById('haaretz').innerHTML ='הָאָרֶץ';
+      return false;
+    }
 
     if( idElement == 1 ){
         document.getElementById('bereshit').innerHTML ='In the begining';
@@ -14,6 +34,26 @@
     }
     if( idElement == 2 ){
         document.getElementById('bara').innerHTML ='created';
+        return false;
+    }
+    if( idElement == 3 ){
+        document.getElementById('Elohim').innerHTML ='Elohim';
+        return false;
+    }
+    if( idElement == 4 ){
+        document.getElementById('et').innerHTML ='אֵת';
+        return false;
+    }
+    if( idElement == 5 ){
+        document.getElementById('hashamaim').innerHTML ='the heavens';
+        return false;
+    }
+    if( idElement == 6 ){
+        document.getElementById('veet').innerHTML ='and אֵת';
+        return false;
+    }
+    if( idElement == 7 ){
+        document.getElementById('haaretz').innerHTML ='the earth';
         return false;
     }
  }
