199cd4ae5cc482f0819d5a767c307166e17ac458
Carlos Garzon <showlove@knights.ucf.edu>
2015-08-20 01:48:12 -0400

    prettied up code added gamatria

--- Bereshit/BereshitDynamic.js
+++ Bereshit/BereshitDynamic.js
@@ -1,61 +1,49 @@
  function changeText(idElement) {
+    //English --> Hebrew
     if( idElement == 1   &&  document.getElementById('bereshit').innerHTML == "In the begining"){
       document.getElementById('bereshit').innerHTML ='בְּרֵאשִׁית';
-      return false;
-    }
+      return false;}
     if( idElement == 2  &&  document.getElementById('bara').innerHTML == "created"){
       document.getElementById('bara').innerHTML ='בָּרָא';
-      return false;
-    }
+      return false;}
     if( idElement == 3  &&  document.getElementById('Elohim').innerHTML == "Elohim"){
       document.getElementById('Elohim').innerHTML ='אֱלֹהִים';
-      return false;
-    }
+      return false;}
     if( idElement == 4  &&  document.getElementById('et').innerHTML == "אֵת"){
       document.getElementById('et').innerHTML ='אֵת';
-      return false;
-    }
+      return false;}
     if( idElement == 5  &&  document.getElementById('hashamaim').innerHTML == "the heavens"){
       document.getElementById('hashamaim').innerHTML ='הַשָּׁמַיִם';
-      return false;
-    }
+      return false;}
     if( idElement == 6  &&  document.getElementById('veet').innerHTML == "and אֵת"){
       document.getElementById('veet').innerHTML ='וְאֵת';
-      return false;
-    }
+      return false;}
     if( idElement == 7  &&  document.getElementById('haaretz').innerHTML == "the earth"){
       document.getElementById('haaretz').innerHTML ='הָאָרֶץ';
-      return false;
-    }
+      return false;}
 
+      //Hebrew --> English
     if( idElement == 1 ){
         document.getElementById('bereshit').innerHTML ='In the begining';
-        return false;
-    }
+        return false;}
     if( idElement == 2 ){
         document.getElementById('bara').innerHTML ='created';
-        return false;
-    }
+        return false;}
     if( idElement == 3 ){
         document.getElementById('Elohim').innerHTML ='Elohim';
-        return false;
-    }
+        return false;}
     if( idElement == 4 ){
         document.getElementById('et').innerHTML ='אֵת';
-        return false;
-    }
+        return false;}
     if( idElement == 5 ){
         document.getElementById('hashamaim').innerHTML ='the heavens';
-        return false;
-    }
+        return false;}
     if( idElement == 6 ){
         document.getElementById('veet').innerHTML ='and אֵת';
-        return false;
-    }
+        return false;}
     if( idElement == 7 ){
         document.getElementById('haaretz').innerHTML ='the earth';
-        return false;
-    }
+        return false;}
  }
 
 $(document).ready(main);
