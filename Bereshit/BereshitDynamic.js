dccd58a9226369ab45128467302e514b8dfea288
Carlos Garzon <showlove@knights.ucf.edu>
2015-08-21 22:44:09 -0400

    Got css to work, added eng/heb buttons, changed from % to pixels

--- Bereshit/BereshitDynamic.js
+++ Bereshit/BereshitDynamic.js
@@ -1,4 +1,4 @@
- function changeText(idElement) {
+ function changeWordLang(idElement){
 //Bereshit 1:1
 
     //English --> Hebrew
@@ -9,7 +9,7 @@
       document.getElementById('bara').innerHTML ='בָּרָא';
       return false;}
     if( idElement == 3  &&  document.getElementById('Elohim').innerHTML == "Elohim"){
-      document.getElementById('Elohim').innerHTML ='אֱלֹהִים';
+      document.getElementById('Elohim').innerHTML ='אֱלֹהִים,';
       return false;}
     if( idElement == 4  &&  document.getElementById('et').innerHTML == "אֵת"){
       document.getElementById('et').innerHTML ='אֵת';
@@ -46,6 +46,32 @@
     if( idElement == 7  &&  document.getElementById('haaretz').innerHTML == "הָאָרֶץ"){
       document.getElementById('haaretz').innerHTML ='the earth';
       return false;}
- }
+ }//endchangeWordLang
+
+ function changeChapterLang(idElement){
+  //change entire text to English
+    
+    //English --> Hebrew
+    if( idElement == 1){
+      document.getElementById('bereshit').innerHTML ='בְּרֵאשִׁית';
+      document.getElementById('bara').innerHTML ='בָּרָא';
+      document.getElementById('Elohim').innerHTML ='אֱלֹהִים';
+      document.getElementById('et').innerHTML ='אֵת';
+      document.getElementById('hashamaim').innerHTML ='הַשָּׁמַיִם';
+      document.getElementById('veet').innerHTML ='וְאֵת';
+      document.getElementById('haaretz').innerHTML ='הָאָרֶץ';
+      return false;}
+
+    //Hebrew --> English
+    if( idElement == 2){
+      document.getElementById('bereshit').innerHTML ='In the begining';
+      document.getElementById('bara').innerHTML ='created';
+      document.getElementById('Elohim').innerHTML ='Elohim';
+      document.getElementById('et').innerHTML ='אֵת';
+      document.getElementById('hashamaim').innerHTML ='the heavens';
+      document.getElementById('veet').innerHTML ='and אֵת';
+      document.getElementById('haaretz').innerHTML ='the earth';
+      return false;}
+ }//endchangeChapterLang
 
 $(document).ready(main);
