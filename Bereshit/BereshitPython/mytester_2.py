65a07c239da2e5edcce55398e34af3c9df83bba2
Carlos Garzon <showlove@knights.ucf.edu>
2015-10-30 11:14:00 -0400

    trying a new system each line of verse is in a list of list, written in mytester_2.py

--- Bereshit/BereshitPython/mytester_2.py
+++ Bereshit/BereshitPython/mytester_2.py
@@ -24,10 +24,6 @@ main_html_functions.header( write_file )
 
 L1 = main_html_functions.L1	#HebTransliteration if word%4 = 3 or 4
 L2 = main_html_functions.L2	#EngAHL				if word%4 = 1 or 2
-prev_newL_Index = 0
-temp = 0
-hebTrans = True
-engLine_End = False
 
 ######################################################################################
 # this loop should write Heb/Transliteration div wrap And the ENG AHL div wrap 
@@ -38,6 +34,8 @@ for index, var_list in enumerate(lines_list):
 
 	line = 0
 	language = L1
+	list_length = len(word_list)
+
 
 	##################################################################################
 	# 7 words per Div/Line. Each for loop writes functions for an entire verse
@@ -53,77 +51,41 @@ for index, var_list in enumerate(lines_list):
 		if ':' in word:
 			chapter_verse = main_html_functions.remColon( word )
 
-		#Switch b/w hebTrans<-->engAHL every new line####################################
-		if hebTrans == True:
+		# write the divs for each word
+		if index2%4 == 3 and index2 != 0: #Print divs for hebrew words
+			main_html_functions.changeWord( write_file, str(index2), chapter_verse, str(line), language, word)
 
-			language = L1
+		# line number: Lines should start at 1
+		# We are on a new line
+		if index2%32 == 0:
 
-			# write the divs for each word
-			if index2%4 == 3 and index2 != 0: #Print divs for hebrew words
-				main_html_functions.changeWord( write_file, str(index2), chapter_verse, str(line), language, word)
+			if index2 != 0:						#DEBUG
+				print word_list[index2] + "(A)," 	#DEBUG A: 	1 Exactly 8 words (new verse next) so verse prints
+															#2 last of 
+															# less than 8 w on line !print
 
-			# line number: Lines should start at 1
-			# We are on a new line
-			if index2%32 == 0:
-				line = line + 1
-				# If !firstLine write end to previous line 
-				if index2 != 0:
-					main_html_functions.end_lang1_2_divWrapper( write_file )
-					#If on a newL that's !firstL then switch hebTrans<-->engAHL
-					hebTrans = False			#NEW
-					temp = index2
-					index2 = prev_newL_Index   	#NEW  is this correct?
-					prev_newL_Index = temp		#Save this for next time we are on a newL
-					engLine_End = False			#Let nxt if know were at beginning of eng line
+			line = line + 1
+			# If !firstLine write end to previous line 
+			if index2 != 0:
+				main_html_functions.end_lang1_2_divWrapper( write_file )
 
-				#If we have no words left theres no use in writing headers
-				#index starts at 0 but len !zero_index
-				#Only do this if we are on a hebrew line
-				if hebTrans == True:
-					if (index2+1) < len(word_list):
-						main_html_functions.lang1_header( write_file )
-						#We want to highlight the first verse
-						if index2 == 0:
-							main_html_functions.lang1_newVerse( write_file, word_list[0] )
-
-		#Switch b/w hebTrans<-->engAHL every new line####################################
-		if hebTrans == False:
-
-			language = L2
-
-			#Print divs for Eng words
-			if index2%4 == 1 and index2 != 0: 
-				main_html_functions.changeWord2( write_file, str(index2), chapter_verse, str(line), language, word)
-
-			# line number: Lines should start at 1
-			# We are on a new line
-			if index2%32 == 0:
-
-				# If !firstLine write end to previous line 
-				if index2 != 0:
-					main_html_functions.end_lang1_2_divWrapper( write_file )
-					# If we are at end of eng line set booleans
-					if engLine_End == True:
-						hebTrans = True			#NEW
-
-				# Next time we hit a new line it should be the end of Eng line
-				engLine_End = True
-
-				#If we have no words left theres no use in writing headers
-				#index starts at 0 but len !zero_index
-				#Only do this if we are on an Eng line
-				if hebTrans == False:
-					if (index2+1) < len(word_list):
-						main_html_functions.lang2_header( write_file,  )
-						#We want to highlight the first verse
-						if index2 == 0:
-							main_html_functions.lang2_newVerse( write_file, word_list[0] )
+			#If we have no words left theres no use in writing headers
+			#index starts at 0 but len !zero_index
+			if (index2+1) < len(word_list):
+				main_html_functions.lang1_header( write_file )
+				#We want to highlight the first verse
+				if index2 == 0:
+					main_html_functions.lang1_newVerse( write_file, word_list[0] )
 
 
 	#exit inner for loop
 	#Last line did NOT have exactly 8 words
 	if index2%32 != 0:
 		main_html_functions.end_lang1_2_divWrapper( write_file )
+
+	line = 0
+	language = L2
+
 
 
 
--- Bereshit/BereshitPython/mytester_3.py
+++ Bereshit/BereshitPython/mytester_3.py
@@ -1,125 +1,59 @@
-def concatenateNext_string_TF( index, lines_list):
-	next_wL_index = index + 1
-	if next_wL_index < len(lines_list):
-		#ThisIs A variable word_list: tric for splitin a string to a list of words
-		problem_var = lines_list[next_wL_index]
-		problem_words2 = problem_var.split()
-		#Check if this is a vere or a glitch where we start in middle of verse
-		if ( (problem_words2[0].isdigit() < 8) and (':' not in problem_words2[0]) ):
-			return True
+# -*- coding: utf-8 -*-
 
-	return False
+import main_html_functions
 
-	#DEBUG: condition_String, real_index, lines_list2
-def concatenateNext_string( index, lines_list, lines_list2, condition_String, real_index):
-	next_wL_index = index + 1
-	if next_wL_index < len(lines_list):
-		#ThisIs A variable word_list: tric for splitin a string to a list of words
-		problem_var = lines_list[next_wL_index]
-		problem_words2 = problem_var.split()
-
-		if ( (problem_words2[0].isdigit() < 8) and (':' not in problem_words2[0]) ):
-			#DEBUG: print problem_words2[0]+condition_String+"[index:"+real_index+"]" #DEBUG
-			#Add a space to the next so that it doesnt join the lastW from this list and firstW from the next
-			#Then Join this line and the next
-			#Then replace word_list
-			lines_list[index+1] = " "+lines_list[index+1]
-			lines_list[index:index+2] = [''.join(lines_list[index:index+2])]
-			word_list = lines_list[index].split()
-			print "w1inConcatFunc("+word_list[0]+","+word_list[-1]+")"
-
-	return word_list
-
-def remColon( chapterVerse ):
-	return chapterVerse.replace(":"," ")
-
-def verseInt( chap_verse_string ):
-	currentVerse = word_list[0]
-	currentVerse = remColon( currentVerse )
-	currV_nums = currentVerse.split()
-	currentVerse = currV_nums[1]
-	currentV_int = int(currentVerse)
-	return currentV_int
-
+#main_html_python.py#
+#################################Part 1###############################################
+######################################################################################
+# This should set up Heb Eng of changeChapterLanguage porition of js
+######################################################################################
 #open the file
-input_file = open( "/Users/carlosgarzon/Desktop/Projects/LearnHtml/Bereshit/BereshitPython/data_chapter1.txt", "r")
+input_file = open( "/Users/carlosgarzon/Desktop/Projects/LearnHtml/Bereshit/BereshitPython/out_main_js.txt", "r")
 
 #read file into a list of lines
-#lines_list --> what we are itterating
 lines_list = input_file.read().splitlines()
-#lines_list2 --> what we are modifying
-lines_list2 = input_file.read().splitlines()
 
 # Close the File stream handler
 input_file.close()
 
 # The OPEN function returns a file object
-write_file = open("/Users/carlosgarzon/Desktop/Projects/LearnHtml/Bereshit/BereshitPython/out_main_js.txt","w")
+write_file = open("/Users/carlosgarzon/Desktop/Projects/LearnHtml/Bereshit/BereshitPython/out_main_html.html","w")
 
-#index_buffer = 0
+#Writess the top portion of html code
+main_html_functions.header( write_file )
 
-# Writing a VARIABLE to file in for loop
-for index, var_list in enumerate(lines_list):
+L1 = main_html_functions.L1	#HebTransliteration if word%4 = 3 or 4
+L2 = main_html_functions.L2	#EngAHL				if word%4 = 1 or 2
 
-	#DEBUG
-	old_word_list = lines_list[index].split()
-	print "w1@currOldIndexLL("+old_word_list[0]+")"
-
-	#gltich: A string that does not beging where verse begins i.e genesis 1:7
-
+######################################################################################
+# this loop should write Heb/Transliteration div wrap And the ENG AHL div wrap 
+######################################################################################
+for index, var_list in enumerate(lines_list): 
 	#tric for splitin a string to a list of words
-	#This will be used if nothing was a glitch
-	#word_list = var_list.split()  <--- how I used to do it
-	word_list = lines_list[index].split()
+	word_list = var_list.split()
 
-	#find the current verse to compare it later
-	if ':' in word_list[0]:
-		currentV_int = verseInt( word_list[0] )
-		#print "This_is_the_verse-->" + str(currentV_int)
+	#chunks of 8 list: split from list of words in each line/verse
+	#Verse is split into 8w chunks and put in a list
+	verse_chunks =[word_list[x:x+32] for x in xrange(0, len(word_list), 32)]
+
+	line = 0
+	language = L1
+	list_length = len(word_list)
+
+	#Finds last word in verse
+	num_lines_in_verse = len(verse_chunks)									#number of chunks								
+	num_w_last_chunk = len(verse_chunks[num_lines_in_verse - 1])			#size of chunk
+	#print verse_chunks[ num_lines_in_verse - 1][ num_w_last_chunk - 1]
+
+	#prints each line: We print 8 lines at a time
+	for lineIndex in range(num_lines_in_verse):
+		for chunksIndex in range( len(verse_chunks[lineIndex]) ):
+			print verse_chunks[lineIndex][chunksIndex] + ",",
+		print "\n\n",
 
 
-	#get number of words in this verse
-	num_word_lists = len(lines_list)
-
-	#If current and next string is a glitch just join current with previous
-	if ( (word_list[0].isdigit() < 8) and (':' not in word_list[0]) ) and concatenateNext_string_TF( index, lines_list):
-		word_list = concatenateNext_string( index - 1, lines_list, lines_list2, "[cur&nextGlitch]", str(index) )
-		#Debug Info
-		new_word_list = lines_list[index].split()
-		print "Cur&NexGlitch-w1@curNewIndex("+new_word_list[0]+")"+"w1@main_W_List("+word_list[0]+")"
-	#Catches if NEXT string is a glitch 
-	elif concatenateNext_string_TF( index, lines_list):
-		word_list = concatenateNext_string( index, lines_list, lines_list2, "[nextGlitch]", str(index) )
-		#Debug Info
-		new_word_list = lines_list[index].split()
-		print "NextGlitch-w1@curNewIndex("+new_word_list[0]+")"+"w1@main_W_List("+word_list[0]+")"
-	#If current string is a glitch join it to previous
-	elif ( (word_list[0].isdigit() < 8) and (':' not in word_list[0]) ):
-		word_list = concatenateNext_string( index - 1, lines_list, lines_list2, "[curGlitch]", str(index) )
-		#Debug Info
-		new_word_list = lines_list[index].split()
-		print "CurrGlitch-w1@curNewIndex("+new_word_list[0]+")"+"w1@main_W_List("+word_list[0]+")"
-
-	#print "EndIndex("+str(index)+")index_buffer("+str(index_buffer)+")"
-
-	#not including C:V and starting on 0 index
-	num_words = len(word_list) - 1
-
-	for index2, word in enumerate(word_list):
-		if len(word_list) > 1:
-			#Bad Data: References, Page Title, (c) 
-			if ( index2 == 0 and word.isdigit() ) or ( word_list[0] == 'The' and word_list[1] == 'Mechanical' ) or ( word_list[1] == 'Jeff' ):
-				print "Bad Data"
-				break
-
-		#DEBUG: we found a new verse. It's printing the index of the outer for loop not this one
-		if ':' in word:
-			print word+ "  " + word_list[1]+"("+str(index)+")"+"\n"
-
-		write_file.write(word+"("+str(index2)+")")
-
-	#exit inner for loop		
-	write_file.write("NumWordsInVerse("+str(num_words)+")"+"\n")
 
 # Close the FILE object in PYTHON
 write_file.close
+
+main_html_functions.w_tail( write_file )
